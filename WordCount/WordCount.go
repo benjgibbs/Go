b623c124139910648e03ba2be95de8119c7ad55a
Ben Gibbs <benjgibbs@gmail.com>
2015-10-05 20:35:41 +0100

    Updating with go-routine one

--- WordCount/WordCount.go
+++ WordCount/WordCount.go
@@ -5,7 +5,86 @@ import (
 	"io/ioutil"
 	"sort"
 	"strings"
+	"time"
 )
+
+const THRESHOLD int = 10000
+const NUM_THREADS int = 4
+
+type WordFreqs struct {
+	Freqs map[string]int
+	Count int
+}
+
+func main() {
+	fmt.Println("ReadingFile...")
+	dat, err := ioutil.ReadFile("/Users/ben/Books/warpeace.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Naive...")
+	start := time.Now()
+	freqs := naive(dat)
+	fmt.Printf("Took %d ms.\n", time.Now().Sub(start)/1000000)
+	printResult(freqs)
+
+	fmt.Println("Parallel...")
+	start = time.Now()
+	freqs = parallel(dat)
+	fmt.Printf("Took %d ms.\n", time.Now().Sub(start)/1000000)
+	printResult(freqs)
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
+
+func parallel(dat []byte) WordFreqs {
+	messages := make(chan WordFreqs)
+	chunk := len(dat) / NUM_THREADS
+	for i := 0; i < NUM_THREADS; i++ {
+		start := i * chunk
+		end := min(start+chunk, len(dat))
+		go func() { messages <- naive(dat[start:end]) }()
+	}
+
+	var result WordFreqs
+	result.Freqs = make(map[string]int)
+	for i := 0; i < NUM_THREADS; i++ {
+		part := <-messages
+		for k, v := range part.Freqs {
+			result.Freqs[k] += v
+		}
+		result.Count += part.Count
+	}
+	return result
+}
+
+func naive(dat []byte) WordFreqs {
+	parts := strings.FieldsFunc(string(dat), func(r rune) bool {
+		switch r {
+		case ' ', '.', '\n', '\r', ':', '*', ';', '(', ')', '%', '£', '\t', ',',
+			'"', '-', '\'', '?', '/', '!':
+			return true
+		}
+		return false
+	})
+	counts := make(map[string]int)
+	for i := range parts {
+		part := parts[i]
+		part = strings.ToLower(part)
+		counts[part]++
+	}
+	wordCount := 0
+	for _, v := range counts {
+		wordCount += v
+	}
+	return WordFreqs{Freqs: counts, Count: wordCount}
+}
 
 type WordFreq struct {
 	Word string
@@ -26,44 +105,19 @@ func (wf ByFreq) Less(i, j int) bool {
 	return wf[i].Freq < wf[j].Freq
 }
 
-func main() {
-
-	fmt.Println("Starting")
-
-	dat, err := ioutil.ReadFile("warpeace.txt")
-	if err != nil {
-		panic(err)
+func printResult(words WordFreqs) {
+	freqs := make([]WordFreq, len(words.Freqs))
+	i := 0
+	for k, v := range words.Freqs {
+		freqs[i] = WordFreq{k, v}
+		i++
 	}
-
-	counts := make(map[string]int)
-	parts := strings.FieldsFunc(string(dat), func(r rune) bool {
-		switch r {
-		case ' ', '.', '\n', '\r', ':', '*', ';', '(', ')', '%', '£', '\t', ',',
-			'"', '-', '\'', '?', '/', '!':
-			return true
+	sort.Sort(ByFreq(freqs))
+	for i := range freqs {
+		freq := freqs[i]
+		if freq.Freq > THRESHOLD {
+			fmt.Printf("%s => %d\n", freq.Word, freq.Freq)
 		}
-		return false
-	})
-
-	for i := range parts {
-		part := parts[i]
-		part = strings.ToLower(part)
-		counts[part]++
 	}
-	frequencies := []WordFreq{}
-	wordCount := 0
-	for k, v := range counts {
-		freq := WordFreq{
-			Word: k,
-			Freq: v,
-		}
-		frequencies = append(frequencies, freq)
-		wordCount += v
-	}
-	sort.Sort(ByFreq(frequencies))
-	for i := range frequencies {
-		freq := frequencies[i]
-		fmt.Printf("%s => %d\n", freq.Word, freq.Freq)
-	}
-	fmt.Printf("Total Words: %d\n", wordCount)
+	fmt.Printf("Total Words: %d.\n", words.Count)
 }
