24966ba65968a5dc4c125f020507ba88e86e1a96
Ben Gibbs <benjgibbs@gmail.com>
2015-10-12 18:46:55 +0100

    Making progress

--- euler/p61.go
+++ euler/p61.go
@@ -8,14 +8,14 @@ import (
  * Takes a 4 digit in and returns the first two
  */
 func front(i int) int {
-	return i % 100
+	return i / 100
 }
 
 /**
  * Takes a 4 digit int and returns the last two
  */
 func back(i int) int {
-	return i / 100
+	return i % 100
 }
 
 func seq(min, max int, next func(n int) int) []int {
@@ -54,84 +54,85 @@ func populate(xs []int, c int, f, b *map[int][]Number) {
 	}
 }
 
-func contains(xs []Number, t Number) bool {
-	for _, x := range xs {
-		if x.class == t.class || x.value == t.value {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
+	fronts := make(map[int][]Number)
+	backs := make(map[int][]Number)
+
 	trias := seq(1000, 9999, func(n int) int { return n * (n - 1) / 2 })
 	squas := seq(1000, 9999, func(n int) int { return n * n })
 	pents := seq(1000, 9999, func(n int) int { return n * (3*n - 1) / 2 })
 	hexes := seq(1000, 9999, func(n int) int { return n * (2*n - 1) })
 	hepts := seq(1000, 9999, func(n int) int { return n * (5*n - 3) / 2 })
 	octos := seq(1000, 9999, func(n int) int { return n * (3*n - 2) })
+
 	fmt.Printf("Sizes: trias=%d, squas=%d, pents=%d, hexes=%d, hepts=%d, octos=%d\n",
 		len(trias), len(squas), len(pents), len(hexes), len(hepts), len(octos))
+	/*
+		populate(trias, 3, &fronts, &backs)
+		populate(squas, 4, &fronts, &backs)
+		populate(pents, 5, &fronts, &backs)
+		populate(hexes, 6, &fronts, &backs)
+		populate(hepts, 7, &fronts, &backs)
+		populate(octos, 8, &fronts, &backs)
+		const CYCLE_LEN = 6
+	*/
+	populate([]int{1122}, 1, &fronts, &backs)
+	populate([]int{2244}, 2, &fronts, &backs)
+	populate([]int{2233}, 2, &fronts, &backs)
+	populate([]int{3344}, 3, &fronts, &backs)
+	populate([]int{3311}, 3, &fronts, &backs)
+	const CYCLE_LEN = 3
 
-	fronts := make(map[int][]Number)
-	backs := make(map[int][]Number)
-	populate(trias, 3, &fronts, &backs)
-	populate(squas, 4, &fronts, &backs)
-	populate(pents, 5, &fronts, &backs)
-	populate(hexes, 6, &fronts, &backs)
-	populate(hepts, 7, &fronts, &backs)
-	populate(octos, 8, &fronts, &backs)
+	// fmt.Printf("Fronts: %d\n", fronts)
+	// fmt.Printf("Backs: %d\n", backs)
+	// fmt.Println("")
 
-	for _, v := range backs {
-		for _, n := range v {
-			fmt.Printf("Checking %d\n", n)
+	for _, front := range fronts {
+		for _, start := range front {
+
 			visited := []Number{}
-			recurse(true, n, &visited, &fronts, &backs)
-			if len(visited) == 6 {
+			visited = recurse(start, visited, fronts)
+
+			if len(visited) == CYCLE_LEN && visited[0].front == visited[CYCLE_LEN-1].back {
 				var sum int
 				for _, x := range visited {
 					sum += x.value
 				}
-				fmt.Printf("Found it: %d (%d)", sum, visited)
+				fmt.Printf("Found it: %d (%d)\n", sum, visited)
 				return
+			}
+			if true || len(visited) == CYCLE_LEN {
+				fmt.Printf("Not found. start=%d, len=%d, route=%d\n", start, len(visited), visited)
+				//fmt.Println()
 			}
 		}
 	}
 }
 
-func recurse(isFront bool, v Number, visited *[]Number, fronts, backs *map[int][]Number) {
-	*visited = append(*visited, v)
-	var next []Number
-	if isFront {
-		next = (*fronts)[v.back]
-	} else {
-		next = (*backs)[v.front]
-	}
+func recurse(v Number, visited []Number, fronts map[int][]Number) []Number {
+	visited = append(visited, v)
+	//fmt.Printf("Recursing. v=%d, visitied=%d\n", v, visited)
+	next := fronts[v.back]
 	for _, f := range next {
-		if !contains(*visited, f) {
-			recurse(!isFront, f, visited, fronts, backs)
+		if eligible(visited, f) {
+			return recurse(f, visited, fronts)
 		}
 	}
+	return visited
 }
 
-//	for _, ord := range permutations.heap([]int{3, 4, 5, 6, 7, 8}) {
-//		for i = 0; i < len(ord)-1; i++ {
-//			x := ord[i]
-//			y := ord[i+1]
-//			var ends *Ends
-//			switch x {
-//			case 3:
-//				ends = &triaEnds
-//			case 4:
-//				ends = &squaEnds
-//			case 5:
-//				ends = &pentEnds
-//			case 6:
-//				ends = &hexeEnds
-//			case 7:
-//				ends = &heptEnds
-//			case 8:
-//				ends = &octaEnds
-//			}
-//			for _,starts := ends.back
-//		}
+func eligible(seen []Number, t Number) bool {
+	for _, x := range seen {
+		if x.class == t.class || x.value == t.value {
+			//fmt.Printf("Not eligbile. %d/%d %d/%d\n", x.class, t.class, x.value, t.value)
+			return false
+		}
+	}
+	//fmt.Printf("Eligible %d\n", t)
+	return true
+}
+
+//[{7140 40 71 6}
+//    {4371 71 43 3}
+//    {4371 71 43 3}
+//       {6943 43 69 7}]
