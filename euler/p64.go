848a563abbdc977515442a424b2731c4b6dd33a8
Ben Gibbs <benjgibbs@gmail.com>
2015-10-14 07:47:12 +0100

    p64 tidy up

--- euler/p64.go
+++ euler/p64.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math"
 )
-
-const RUNAWAY_BOUND = 300
 
 type iteration struct {
 	m, d, a int
@@ -16,32 +13,24 @@ func equal(i, j iteration) bool {
 	return i.a == j.a && i.d == j.d && i.m == j.m
 }
 
-func contFracLen(s int) (bool, int) {
+func contFracLen(s int) int {
 
 	a0 := int(math.Floor(math.Sqrt(float64(s))))
-
 	m, d, a := 0, 1, a0
 	previous := []iteration{}
+
 	for {
 		m = d*a - m
 		d = (s - (m * m)) / d
 		a = (a0 + m) / d
-		if len(previous) > RUNAWAY_BOUND {
-			return false, 0
-		}
 		it := iteration{m, d, a}
 		for i, x := range previous {
 			if equal(x, it) {
-				return true, len(previous) - i
+				return len(previous) - i
 			}
 		}
 		previous = append(previous, it)
 	}
-}
-
-func check(i int) {
-	t, n := contFracLen(i)
-	fmt.Printf("contFracLen(%d)=%t,%d\n", i, t, n)
 }
 
 func countOddsUpTo(bound int) int {
@@ -54,11 +43,7 @@ func countOddsUpTo(bound int) int {
 	for i := 2; i <= bound; i++ {
 		_, sq := squares[i]
 		if !sq {
-			s, l := contFracLen(i)
-			if !s {
-				log.Fatal("Bound is not high enough")
-			}
-			if l%2 == 1 {
+			if contFracLen(i)%2 == 1 {
 				oddCount++
 			}
 		}
