dd538a36d928e7865710f79fa45abcb12095d621
Ben Gibbs <benjgibbs@gmail.com>
2015-10-16 07:47:46 +0100

    Searching not iterating

--- euler/p387.go
+++ euler/p387.go
@@ -18,51 +18,40 @@ func isHarshad(x uint64) bool {
 	return x%sum == 0
 }
 
-func isRTH(x uint64) bool {
-	for x > 0 {
-		if !isHarshad(x) {
-			return false
-		}
-		x = x / 10
-	}
-	return true
-}
-
 func isStrong(x uint64, s *Sieve) bool {
 	sum := sumOfDigits(x)
 	div := x / sum
 	return s.isPrime(uint64(div))
 }
 
-func isRT(x uint64) bool {
-	for x > 0 {
-		if !isHarshad(x) {
-			return false
-		}
-		x = x / 10
-	}
-	return true
-}
-
-func isSRTHP(x uint64, s *Sieve) bool {
-	if !s.isPrime(uint64(x)) {
-		return false
-	}
-	x = x / 10
-	if !isStrong(x, s) {
-		return false
-	}
-	return isRT(x)
-}
-
 func p387() {
-	const limit = uint64(100)
-	s := createSieve(limit + 1)
+	s := createSieve(10000001)
 	sum := uint64(0)
-	for i := uint64(10); i < limit*limit; i++ {
-		if isSRTHP(i, s) {
+	harshads := []uint64{}
+
+	for i := uint64(10); i < 100; i++ {
+		if isHarshad(i) {
+			harshads = append(harshads, i)
+		}
+		if s.isPrime(i) && isStrong(i/10, s) {
 			sum += i
 		}
 	}
-	fmt.Printf("Total is: %d\n", sum)
+	for i := 0; i < (14 - 2); i++ {
+		nextHarshads := []uint64{}
+		for _, x := range harshads {
+			for j := uint64(0); j < 10; j++ {
+				n := x*10 + j
+				if isHarshad(n) {
+					nextHarshads = append(nextHarshads, n)
+				}
+				if s.isPrime(n) && isStrong(n/10, s) {
+					sum += n
+				}
+			}
+		}
+		harshads = nextHarshads
+	}
+
+	fmt.Printf("Total is: %d expected (696067597313468)\n", sum)
 }
