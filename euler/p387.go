d8bd056d9a8175c986e3ebe99bf5c75e090064d2
Ben Gibbs <benjgibbs@gmail.com>
2015-10-16 07:58:53 +0100

    Tuning - runs in real 0m2.067s

--- euler/p387.go
+++ euler/p387.go
@@ -19,13 +19,14 @@ func isHarshad(x uint64) bool {
 }
 
 func isStrong(x uint64, s *Sieve) bool {
+	x = x / 10
 	sum := sumOfDigits(x)
 	div := x / sum
-	return s.isPrime(uint64(div))
+	return s.isPrime(div)
 }
 
 func p387() {
-	s := createSieve(10000001)
+	s := createSieve(10000000)
 	sum := uint64(0)
 	harshads := []uint64{}
 
@@ -33,11 +34,12 @@ func p387() {
 		if isHarshad(i) {
 			harshads = append(harshads, i)
 		}
-		if s.isPrime(i) && isStrong(i/10, s) {
+		if isStrong(i, s) && s.isPrime(i) {
 			sum += i
 		}
 	}
 	for i := 0; i < (14 - 2); i++ {
+		fmt.Printf("Searching %d harshads. i=%d, sum=%d \n", len(harshads), i, sum)
 		nextHarshads := []uint64{}
 		for _, x := range harshads {
 			for j := uint64(0); j < 10; j++ {
@@ -45,13 +47,12 @@ func p387() {
 				if isHarshad(n) {
 					nextHarshads = append(nextHarshads, n)
 				}
-				if s.isPrime(n) && isStrong(n/10, s) {
+				if isStrong(n, s) && s.isPrime(n) {
 					sum += n
 				}
 			}
 		}
 		harshads = nextHarshads
 	}
-
-	fmt.Printf("Total is: %d expected (696067597313468)\n", sum)
+	fmt.Printf("Total is: %d\n", sum)
 }
