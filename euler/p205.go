ecccf98261d920277bd553f5c8ada0dc98d068fb
Ben Gibbs <benjgibbs@gmail.com>
2015-10-28 20:28:56 +0000

    tidy up using recursion

--- euler/p205.go
+++ euler/p205.go
@@ -2,58 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
+func throw(numSides, n, cnt int, acc map[int]int) {
+	if n == 0 {
+		acc[cnt] = acc[cnt] + 1
+	} else {
+		for i := 1; i <= numSides; i++ {
+			throw(numSides, n-1, cnt+i, acc)
+		}
+	}
+}
+
 func p205() {
 	// p can score from 9 to 36
-	tot := 0
 	pScoresSum := make(map[int]int)
-	for p1 := 1; p1 <= 4; p1++ {
-		for p2 := 1; p2 <= 4; p2++ {
-			for p3 := 1; p3 <= 4; p3++ {
-				for p4 := 1; p4 <= 4; p4++ {
-					for p5 := 1; p5 <= 4; p5++ {
-						for p6 := 1; p6 <= 4; p6++ {
-							for p7 := 1; p7 <= 4; p7++ {
-								for p8 := 1; p8 <= 4; p8++ {
-									for p9 := 1; p9 <= 4; p9++ {
-										idx := p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9
-										pScoresSum[idx] = pScoresSum[idx] + 1
-										tot += 1
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+	throw(4, 9, 0, pScoresSum)
 	pScoresProb := make(map[int]float64)
+	pTot := math.Pow(4.0, 9.0)
 	for id, cnt := range pScoresSum {
-		pScoresProb[id] = float64(cnt) / float64(tot)
+		pScoresProb[id] = float64(cnt) / pTot
 	}
-	tot = 0
-	cScoresTot := make(map[int]int)
-	for c1 := 1; c1 <= 6; c1++ {
-		for c2 := 1; c2 <= 6; c2++ {
-			for c3 := 1; c3 <= 6; c3++ {
-				for c4 := 1; c4 <= 6; c4++ {
-					for c5 := 1; c5 <= 6; c5++ {
-						for c6 := 1; c6 <= 6; c6++ {
-							idx := c1 + c2 + c3 + c4 + c5 + c6
-							cScoresTot[idx] = cScoresTot[idx] + 1
-							tot++
-						}
-					}
-				}
-			}
-		}
-	}
+	cScoresSum := make(map[int]int)
+	throw(6, 6, 0, cScoresSum)
+	cTot := math.Pow(6.0, 6.0)
 	cScoresProb := make(map[int]float64)
-	for id, cnt := range cScoresTot {
-		cScoresProb[id] = float64(cnt) / float64(tot)
+	for id, cnt := range cScoresSum {
+		cScoresProb[id] = float64(cnt) / cTot
 	}
 
 	// P(p wins)
