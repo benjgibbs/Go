a7315fb6b8d368bc920a926d8156ee8aca08b1f0
Ben Gibbs <benjgibbs@gmail.com>
2015-11-02 07:12:39 +0000

    moved permutations to the package

--- euler/utils.go
+++ euler/utils.go
@@ -39,3 +39,27 @@ func contains(x uint64, ys []uint64) (bool, int) {
 	}
 	return false, -1
 }
+
+func Heap(xs []int) [][]int {
+	var result [][]int
+	heap(len(xs), xs, &result)
+	return result
+}
+
+func heap(n int, xs []int, res *[][]int) {
+	if n == 1 {
+		out := make([]int, len(xs))
+		copy(out, xs)
+		*res = append(*res, out)
+	} else {
+		for i := 0; i < n-1; i += 1 {
+			heap(n-1, xs, res)
+			if n%2 == 0 {
+				xs[i], xs[n-1] = xs[n-1], xs[i]
+			} else {
+				xs[0], xs[n-1] = xs[n-1], xs[0]
+			}
+		}
+		heap(n-1, xs, res)
+	}
+}
