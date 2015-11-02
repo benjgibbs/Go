b24e7c1919a7f441d35a5ef52dd9e58dcc05fb0d
Ben Gibbs <benjgibbs@gmail.com>
2015-11-02 07:41:26 +0000

    Correct solution using randomisation

--- euler/p345.go
+++ euler/p345.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"sort"
 	"strings"
@@ -19,6 +20,19 @@ func removeSpaces(ss []string) []string {
 	return result
 }
 
+func p345() {
+	file, err := os.Open("p345_matrix.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(bufio.NewReader(file))
+	orderedRemoval(scanner)
+}
+
+type Entry struct {
+	val, row, col int
+}
+
 func (e Entries) Len() int {
 	return len(e)
 }
@@ -31,9 +45,6 @@ func (e Entries) Swap(i, j int) {
 	e[i], e[j] = e[j], e[i]
 }
 
-type Entry struct {
-	val, row, col int
-}
 type Entries []Entry
 
 func cont(x int, xs []int) bool {
@@ -45,13 +56,7 @@ func cont(x int, xs []int) bool {
 	return false
 }
 
-func p345() {
-	file, err := os.Open("p345_given.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(bufio.NewReader(file))
-
+func orderedRemoval(scanner *bufio.Scanner) {
 	var entries Entries
 	rows := 0
 	count := 0
@@ -66,19 +71,30 @@ func p345() {
 		}
 		rows++
 	}
-	fmt.Printf("row=%d, numCols=%d\n", rows, count/rows)
+	fmt.Printf("Matrix dimensions: rows=%d, cols=%d\n", rows, count/rows)
 	sort.Sort(entries)
+	best := 0
+	for i := 0; i < 1000; i++ {
+		sum := run(entries, rows, count/rows)
+		if sum > best {
+			fmt.Printf("sum=%d, i=%d\n", sum, i)
+			best = sum
+		}
+	}
+
+}
+
+func run(entries Entries, rows, cols int) int {
 	seenRows := []int{}
 	seenCols := []int{}
 	sum := 0
-	for i := 0; len(seenRows) < rows && len(seenCols) < count/rows; i++ {
-		e := entries[i]
-		if !cont(e.row, seenRows) && !cont(e.col, seenCols) {
+	for i := 0; len(seenRows) < rows && len(seenCols) < cols; i++ {
+		e := entries[i%(rows*cols)]
+		if !cont(e.row, seenRows) && !cont(e.col, seenCols) && rand.Intn(3) != 0 {
 			seenRows = append(seenRows, e.row)
 			seenCols = append(seenCols, e.col)
 			sum += e.val
-			fmt.Printf("Adding %d\n", e)
 		}
 	}
-	fmt.Printf("Sum: %d\n", sum)
+	return sum
 }
