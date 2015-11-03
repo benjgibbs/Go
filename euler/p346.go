1f10046691902044bd88451281f1558767cdcee9
Ben Gibbs <benjgibbs@gmail.com>
2015-11-03 07:26:27 +0000

    Checking out git mode zsh

--- euler/p346.go
+++ euler/p346.go
@@ -4,36 +4,55 @@ import (
 	"fmt"
 )
 
-func isBaseNRepUnit(n, num uint) bool {
-	//fmt.Printf("num=%d\n", num)
-	cont := num - 1
-	if cont == 0 {
-		return true
-	}
-	if cont%n > 0 {
-		//fmt.Printf("Fail: %d\n", cont%6)
-		return false
-	}
-	return isBaseNRepUnit(n, cont/n)
+func nextRepUnit(base, last uint) {
+	last*base + 1
+}
+
+func p346() {
+	memoryBound()
 }
 
 func repUnit(base, limit uint) []uint {
 	var res []uint
-	for i := 1; i < limit; i = i + base + 1 {
+	for i := uint(1); i < limit; i = i*base + 1 {
 		res = append(res, i)
 	}
 	return res
 }
 
-func p346() {
-	var num uint = 0
-	for i := uint(0); i < 8; i++ {
-		num = num | 1<<i
-		for j := uint(3); j < num; j++ {
-			if isBaseNRepUnit(j, num) {
-				fmt.Println("Strong repunit: ", num)
+func memoryBound() {
+	const limit = 1000
+	var repUnits [][]uint
+	repUnits = make([][]uint, limit)
+
+	for i := 2; i < limit; i++ {
+		repUnits[i] = repUnit(uint(i), limit)
+	}
+	sum := uint(0)
+	count := 0
+	for n := uint(1); n < limit; n++ {
+		if check(repUnits, n) {
+			sum += n
+			count++
+		}
+	}
+	fmt.Printf("sum=%d\n", sum)
+	fmt.Printf("count=%d\n", count)
+}
+
+func check(repUnits [][]uint, n uint) bool {
+	nCount := 0
+	for i := 0; i < len(repUnits); i++ {
+		//fmt.Printf("repUnits[%d]: %d\n", i, repUnits[i])
+		for _, x := range repUnits[i] {
+			if n == x {
+				nCount++
+			}
+			if nCount == 2 {
+				//fmt.Printf("RepUnit: %d\n", n)
+				return true
 			}
 		}
 	}
-
+	return false
 }
